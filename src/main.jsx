f0bf5f8050e94d837bc44a86a145fc7cf5374fe0
DaggieBlanqx <daggieblanqx@gmail.com>
2021-11-04 00:36:10 +0300

    3 files modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,21 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import App from "./App";
-import "./main.css";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
-let rootElement = document.getElementById("app");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
